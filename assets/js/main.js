7a668353a90056fdb84e5885dac2d49a1f844a8b
Drumshtick <ekodnar@outlook.com>
2021-10-23 02:19:55 +0000

    Moved layout draft to new file 'layout.html', linked main.js to index.html

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+});
+const data = [];
+const options = {
+  width: 100,
+  height: 100,
+  barValueLocation: center,
+};
+const element = "main";
+function drawBarChart(data, options, element) {
+
+}
