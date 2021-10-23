65e92e17a1192758411d3929e937648f59106a8d
Drumshtick <ekodnar@outlook.com>
2021-10-23 02:32:40 +0000

    Added all options to options object

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,19 @@
 $(document).ready(function(){
 
 });
-const data = [];
+const data = [40, 80, 5, 25, 32];
 const options = {
   width: 100,
   height: 100,
   barValueLocation: center,
+  barColour: "#0099cc",
+  labelColor: 'black',
+  barSpacing: 'auto',
+  barChartAxes: 'auto',
+  barYMax: 'auto',
+  barYMid: 'auto',
+  titleFont: 'auto',
+  titleFontSize: 'auto',
 };
 const element = "main";
 function drawBarChart(data, options, element) {
